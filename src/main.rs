87d3076d46c638e0e9c6593842725c1ed3f77e04
Gregory Porter <greg.e.porter@gmail.com>
2024-11-22 11:03:47 -0600

    Added new files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use chrono::{NaiveDate};
+
+//struct is required to hold the object written to the csv
 struct ListItem {
     url: Option<String>,
     title: Option<String>,
@@ -5,6 +8,7 @@ struct ListItem {
     date: Option<String>,
 }
 
+//I believe I wrote this for either debugging or maybe it's require to write to the csv
 impl std::fmt::Display for ListItem {
     fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         write!(
@@ -16,14 +20,20 @@ impl std::fmt::Display for ListItem {
 }
 
 fn main() {
-    let base_url: &str = "https://www.kshb.com/search?q=abortion%20AND%20ballot%20AND%20Kansas&p=";
+    //let base_url: &str = "https://www.kshb.com/search?q=abortion%20AND%20ballot%20AND%20Kansas&p=";
+    //let base_url: &str = "https://www.wxyz.com/search?q=abortion%20AND%20ballot%20AND%20Michigan&p=";
+    let base_url: &str = "https://www.lex18.com/search?q=abortion%20AND%20ballot%20AND%20Kentucky&p=";
 
-    // current iteration
+    // current iteration - to turn this into a terminal argument, maybe
     let mut i = 1;
-    // max number of iterations allowed
-    let max_iterations = 18058; //is the total
 
-    let path = std::path::Path::new("kshb.csv");
+    // max number of iterations allowed
+    let max_iterations = 11849;
+
+    //Rust is really particular about who owns a variable
+    //so we format the string, clone it, and then send the pointer to path and writer
+    let binding = &format!("lex18-{}.csv",i).clone();
+    let path = std::path::Path::new(binding);
     let mut writer = csv::Writer::from_path(path).unwrap();
 
     // append the header to the CSV
@@ -37,8 +47,7 @@ fn main() {
 
         let news_response = reqwest::blocking::get(format!("{base_url}{i}"));
         //get the HTML content from the request response
-        //and print it
-        //
+
         let html_content = news_response.unwrap().text().unwrap();
 
         let document = scraper::Html::parse_document(&html_content);
@@ -46,13 +55,21 @@ fn main() {
         let html_list_item_selector = scraper::Selector::parse(".ListItem ").unwrap();
         let list_items = document.select(&html_list_item_selector);
 
+        //loop to get the items in the page
         for list_item in list_items {
             let url = list_item.value().attr("href").map(str::to_owned);
 
-            let date = list_item
-                .select(&scraper::Selector::parse(".ListItem-date").unwrap())
-                .next()
-                .map(|date| date.text().collect::<String>());
+            let Ok((raw_date, _reminder)) = NaiveDate::parse_and_remainder(
+                list_item
+                    .select(&scraper::Selector::parse(".ListItem-date").unwrap())
+                    .next()
+                    .unwrap()
+                    .attr("data-timestamp")
+                    .unwrap(),
+                "%Y-%m-%d"
+            ) else { todo!() } ;
+
+            let date: Option<String> = Some(raw_date.to_string());
 
             let author_raw = list_item
                 .select(&scraper::Selector::parse(".ListItem-author").unwrap())
